965b1429890295a3cdf9e33e605b49c0135446c4
cabrera-evil <douglascabrera.dev@gmail.com>
2022-04-28 13:43:24 -0600

    Delete: Replit settings

--- index.js
+++ index.js
@@ -2,22 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 const TOKEN = process.env['TOKEN']
 
-//Keep the server online
-const keepAlive = require('./server');
-const Monitor = require('ping-monitor');
- 
-keepAlive();
-const monitor = new Monitor({
-    website: 'https://Morty-Discord.cabreraevil.repl.co',
-    title: 'Main',
-    interval: 30 // minutes
-});
-
-monitor.on('up', (res) => console.log(`${res.website} server online.`));
-monitor.on('down', (res) => console.log(`${res.website} server down - ${res.statusMessage}`));
-monitor.on('stop', (website) => console.log(`${website} server stopped.`) );
-monitor.on('error', (error) => console.log(error));
-
 client.on('ready', () =>{
     console.log(`Bot ready as: ${client.user.tag}`);
     client.user.setStatus("online");
